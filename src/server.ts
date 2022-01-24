0741b2d60419c5b293c5cf576a06e017a8676e38
FlpSlva <felipinhoznoroeste@gmail.com>
2022-01-24 16:07:26 -0300

    calling/importing error/express-errors

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,31 @@
 import "reflect-metadata"
-import express, { json }  from "express";
-import { router } from './routes';
+import express, { Request, Response, NextFunction }  from "express";
+import "express-async-errors"
 import "./database"
 import "./shared/container"
+import { AppError } from "./errors/AppError";
+
+import { router } from './routes';
 
 const app = express()
 const port = 3333;
 
-app.use(json())
+app.use(express.json())
 app.use(router);
+
+app.use(
+    (err: Error, req: Request, res: Response, next: NextFunction) => {
+    if(err instanceof AppError){
+        return res.status(err.statusCode).json({
+            message: err.message
+        });
+    }
+
+    return res.status(500).json({
+        status: "error",
+        message: `Internal Server Error - ${err.message}`,
+    });
+})
 
 
 app.listen(port, () => console.log("server is running on port", port));
